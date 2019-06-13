503159f7c2c1d986b4e47fc056d4a9b39d1af354
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-06-12 21:38:16 -0700

    customizing

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# Simpsons Clicky Game
+# Crypto Clicky Game
 
-#### Test your memory with your favorite friends from *The Simpsons*.
 
-#### Click on each character once to win the game. But don't click on a character twice, or it's game over for you!
+#### Click on each crypto once to win the game. But don't click on a crypto twice, or it's game over for you!
 
-#### Built with React. Check it out [here](https://wllm-chndlr.github.io/clicky-game/).
+#### Built with React. 
 
-![screenshot](public/screenshot.png)
--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
     this.setState({
       currentScore: 0,
       topScore: this.state.topScore,
-      rightWrong: "Glaven!",
+      rightWrong: "Wrong!!",
       clicked: []
     });
     this.handleShuffle();
@@ -70,15 +70,15 @@ class App extends Component {
     return (
       <Wrapper>
         <Nav
-          title="Simpsons Clicky Game"
+          title="Crypto Clicky Game"
           score={this.state.currentScore}
           topScore={this.state.topScore}
           rightWrong={this.state.rightWrong}
         />
 
         <Title>
-          Try to click on each character, but don't hit any duplicates, or
-          we'll release the hounds!!!
+          Try to click on each crypto, but don't hit any duplicates, or
+          else!
         </Title>
 
         <Container>
